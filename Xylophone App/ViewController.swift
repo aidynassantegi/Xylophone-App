7e5722301bf6a9deca6243bc3e5d386305dff864
Aidyn Assan <56201005+aidynassantegi@users.noreply.github.com>
2022-01-24 15:27:45 +0600

    Added sounds and introducing app

--- Xylophone App/ViewController.swift
+++ Xylophone App/ViewController.swift
@@ -6,14 +6,28 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    
+    var player: AVAudioPlayer!
 
+    @IBAction func keyPressed(_ sender: UIButton) {
+        playSound(soundName: sender.titleLabel!.text!)
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    func playSound(soundName: String) {
+        let url = Bundle.main.url(forResource: soundName, withExtension: "wav")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player.play()
+                
+    }
 
 }
 
